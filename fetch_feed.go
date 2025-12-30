dbd8871e0904c7d6b4348cad0cbd06c0d75ad5a0
donna <donnamarijne+gh@gmail.com>
2025-12-30 16:05:47 +0000

    ch3l1 (cleanup)

--- fetch_feed.go
+++ fetch_feed.go
@@ -43,25 +43,11 @@ func fetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
 		return nil, fmt.Errorf("failed to read a response body: %w", err)
 	}
 
-	fmt.Printf("%s\n\n", string(buf))
-
 	var rssFeed RSSFeed
 	err = xml.Unmarshal(buf, &rssFeed)
 	if err != nil {
 		return nil, fmt.Errorf("failed to unmarshal XML from request body: %w", err)
 	}
 
-	// decodeRSSFeed(&rssFeed)
-
 	return &rssFeed, nil
 }
-
-// func decodeRSSFeed(rssFeed *RSSFeed) {
-// 	rssFeed.Channel.Title = html.UnescapeString(rssFeed.Channel.Title)
-// 	rssFeed.Channel.Description = html.UnescapeString(rssFeed.Channel.Description)
-//
-// 	for _, item := range rssFeed.Channel.Item {
-// 		item.Title = html.UnescapeString(item.Title)
-// 		item.Description = html.UnescapeString(item.Description)
-// 	}
-// }
